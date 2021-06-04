d45c1c0c360cb64d55b1286e7fc08caca34429d9
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-06-04 12:36:20 +0200

    Unnecessary returns

--- packages/transformers/js/wasm/src/lib.rs
+++ packages/transformers/js/wasm/src/lib.rs
@@ -1,14 +1,13 @@
 extern crate parcel_js_swc_core;
 
 use js_sys::Error;
-use serde_wasm_bindgen;
+use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn transform(config_val: JsValue) -> Result<JsValue, JsValue> {
-  let config: parcel_js_swc_core::Config =
-    serde_wasm_bindgen::from_value(config_val).map_err(|err| JsValue::from(err))?;
+  let config: parcel_js_swc_core::Config = from_value(config_val).map_err(JsValue::from)?;
   let result = parcel_js_swc_core::transform(config)
     .map_err(|e| Error::from(JsValue::from_str(&e.to_string())))?;
-  Ok(serde_wasm_bindgen::to_value(&result).map_err(|err| JsValue::from(err))?)
+  to_value(&result).map_err(JsValue::from)
 }
